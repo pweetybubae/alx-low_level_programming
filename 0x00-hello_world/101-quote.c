e4b21a043518e9e047258e82be73ed9c186bba4f
pweetybubae <ebubeogbuke@outlook.com>
2023-02-16 18:33:45 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry
- * Return: 1
- */
+* main - Entry
+* Return: 1 (Success)
+*/
 int main(void)
-{
-	write(1, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
