599eaed1177bfac8a0bd6f340345a19968124303
pweetybubae <ebubeogbuke@outlook.com>
2023-02-17 12:11:06 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets in lower case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
