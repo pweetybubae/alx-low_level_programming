f4e4ffed89cab545e4147def92769f677595cc42
pweetybubae <ebubeogbuke@outlook.com>
2023-03-10 16:59:32 -0800

    new

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,4 +1,8 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 /**
  * _islower - checks for lowercase character
  * @c: the character to check
--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,4 +1,8 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 /**
  * _isalpha - checks for alphabetic character
